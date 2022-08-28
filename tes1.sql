fc6bc24eb0e63e8840a1950fed6ff02a808386bc
user <user@usernoMacBook-Pro.local>
2022-08-28 23:13:38 +0900

    tes1 thu

---
+++ tes1.sql
@@ -0,0 +1,37 @@
+-- Exercise 1: Tiếp tục với Database Testing System
+--  Question 1: Thêm ít nhất 10 record vào mỗi table
+
+
+
+-- Question 2: lấy ra tất cả các phòng ban
+SELECT * FROM Department;
+
+
+-- Question 3: lấy ra id của phòng ban "Sale"
+
+SELECT * FROM Department WHERE DepartmentName="Sale";
+
+-- Question 4: lấy ra thông tin account có full name dài nhất
+SELECT ;
+
+
+-- Question 5: Lấy ra thông tin account có full name dài nhất và thuộc phòng ban có id = 3
+
+SELECT * FROM `account` ;
+
+-- Question 6: Lấy ra tên group đã tham gia trước ngày 20/12/2019
+
+SELECT * FROM `group` WHERE GroupName <'20/12/2019';
+
+-- Question 7: Lấy ra ID của question có >= 4 câu trả lời
+-- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút và được tạo trước ngày 20/12/2019
+
+-- Question 9: Lấy ra 5 group được tạo gần đây nhất
+-- Question 10: Đếm số nhân viên thuộc department có id = 2
+-- Question 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "D" và kết thúc bằng chữ "o"
+-- Question 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
+-- Question 13: Xóa tất cả các question có nội dung bắt đầu bằng từ "câu hỏi"
+-- email thành loc.nguyenba@vti.com.vn
+-- Question 15: update account có id = 5 sẽ thuộc group có id = 4
+
+
